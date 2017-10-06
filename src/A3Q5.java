6d7586e0e0ec0e1cb8e4a63a1f1f63d789e4dfa8
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-10-06 10:14:00 -0400

    School Oct.6

--- src/A3Q5.java
+++ src/A3Q5.java
@@ -13,6 +13,40 @@ public class A3Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        A3Q5 test = new A3Q5();
+        String[] words = {"ahat", "agd", "awe", "aqe", "aad"};
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].toLowerCase();
+        }
+        System.out.println("BEFORE SORT");
+        for (int j = 0; j < words.length; j++) {
+            System.out.println(words[j]);
+        }
+
+        test.insertionAlphabet(words);
+
+        System.out.println("AFTER SORT");
+        for (int j = 0; j < words.length; j++) {
+            System.out.println(words[j]);
+        }
+
+    }
+/**
+ * Method to alphabetize the words
+ * @param words 
+ */
+    public void insertionAlphabet(String[] words) {
+        //Go throguth the psoitions of the array except for the last place
+        for (int i = 0; i < words.length - 1; i++) {
+            int pos = i;
+            //Check if the position is greater than o and if the first word is greater than the second
+            while (pos >= 0 && words[pos].compareTo(words[pos + 1]) > 0) {
+                //Swap the positions of the words constantly until its in the right position
+                String temp = words[pos];
+                words[pos] = words[pos + 1];
+                words[pos + 1] = temp;
+                pos--;
+            }
+        }
     }
 }
